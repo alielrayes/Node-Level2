5e1c8b1c6e26af315424c55c0e4eca9468316124
alielrayes <engali7assan@yahoo.com>
2023-08-12 18:29:20 +0300

    lesson17

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,8 +2,6 @@ const User = require("../models/customerSchema");
 var moment = require("moment");
 
 const user_index_get = (req, res) => {
-  // result ==> array of objects
-  console.log("--------------------------------------------");
   User.find()
     .then((result) => {
       res.render("index", { arr: result, moment: moment });
--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -1,33 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
-const AuthUser = require("../models/authUser");
-const bcrypt = require("bcrypt");
-var jwt = require("jsonwebtoken");
+
 var { requireAuth } = require("../middleware/middleware");
 const { checkIfUser } = require("../middleware/middleware");
 const { check, validationResult } = require("express-validator");
+const authController = require("../controllers/authController");
 
 router.get("*", checkIfUser);
 
 // Level 2
 
-router.get("/signout", (req, res) => {
-  res.cookie("jwt", "", { maxAge: 1 });
-  res.redirect("/");
-});
+router.get("/signout", authController.get_signout);
 
-router.get("/", (req, res) => {
-  res.render("welcome");
-});
+router.get("/login", authController.get_login);
 
-router.get("/login", (req, res) => {
-  res.render("auth/login");
-});
-
-router.get("/signup", (req, res) => {
-  res.render("auth/signup");
-});
+router.get("/signup", authController.get_signup);
 
 router.post(
   "/signup",
@@ -38,54 +26,12 @@ router.post(
       "Password must be at least 8 characters with 1 upper case letter and 1 number"
     ).matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/),
   ],
-  async (req, res) => {
-    try {
-      // check validation (email & password)
-      const objError = validationResult(req);
-      if (objError.errors.length > 0) {
-        return res.json({ arrValidationError: objError.errors });
-      }
-
-      // check if the email already exist
-      const isCurrentEmail = await AuthUser.findOne({ email: req.body.email });
-      if (isCurrentEmail) {
-        return res.json({ existEmail: "Email already exist" });
-      }
-
-      // create new user and login
-      const newUser = await AuthUser.create(req.body);
-      var token = jwt.sign({ id: newUser._id }, "c4a.dev");
-      res.cookie("jwt", token, { httpOnly: true, maxAge: 86400000 });
-      res.json({ id: newUser._id });
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  authController.post_signup
 );
 
-router.post("/login", async (req, res) => {
-  try {
-    const loginUser = await AuthUser.findOne({ email: req.body.email });
+router.post("/login", authController.post_login);
 
-    if (loginUser == null) {
-      res.json({ notFoundEmail: "Email not found, try to sign up" });
-    } else {
-      const match = await bcrypt.compare(req.body.password, loginUser.password);
-      if (match) {
-        var token = jwt.sign({ id: loginUser._id }, "c4a.dev");
-        res.cookie("jwt", token, { httpOnly: true, maxAge: 86400000 });
-        res.json({ id: loginUser._id });
-      } else {
-        res.json({
-          passwordError: `incorrect password for  ${req.body.email}`,
-        });
-      }
-    }
-  } catch (error) {
-    console.log(error);
-  }
-});
-
+router.get("/", authController.get_welcome);
 // Level 1
 // GET Requst
 router.get("/home", requireAuth, userController.user_index_get);
