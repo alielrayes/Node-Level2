da364316cd73b307703dba850b8d99a970874cfa
alielrayes <engali7assan@yahoo.com>
2023-08-13 08:36:50 +0300

    lesson19

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 const mongoose = require("mongoose");
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
